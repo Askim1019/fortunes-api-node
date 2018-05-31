4235ea4b82ef55eb266de9d9b76db942247fd71f
Alex Kim <askim1019@gmail.com>
2018-05-30 22:51:06 -0500

    Get random fortune to render json

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fortunes = require('./data/fortunes');
+
+
+const app = express();
+
+app.get('/fortunes', (req, res) => {
+    res.json(fortunes);
+});
+
+app.get('/fortunes/random', (req, res) => {
+   console.log('requesting random fortune');
+
+   const random_index = Math.floor(Math.random() * fortunes.length);
+
+   const r_fortune = fortunes[random_index];
+
+   res.json(r_fortune);
+});
+
+module.exports = app;
