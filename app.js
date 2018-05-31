03a017d3a33d3137fa31ab91ef973987963f02b1
Alex Kim <askim1019@gmail.com>
2018-05-30 23:53:34 -0500

    Add fortunes find to find by id paramter

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ app.get('/fortunes', (req, res) => {
 });
 
 app.get('/fortunes/random', (req, res) => {
-   console.log('requesting random fortune');
 
-   const random_index = Math.floor(Math.random() * fortunes.length);
+   res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
 
-   const r_fortune = fortunes[random_index];
-
-   res.json(r_fortune);
+app.get('/fortunes/:id', (req, res) => {
+    res.json(fortunes.find(f => f.id == req.params.id));
 });
 
 module.exports = app;
